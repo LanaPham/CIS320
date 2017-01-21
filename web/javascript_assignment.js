a02ec6586df51f4a70c4693ae624a27a898f574b
DESKTOP-0LVI7CE\Lana <lana.pham@my.simpson.edu>
2017-01-20 20:08:19 -0600

    Javascript assignment completed. Jan 20th, 2016

--- web/javascript_assignment.js
+++ web/javascript_assignment.js
@@ -1,31 +1,69 @@
 /**
- * Created by Lana on 1/19/2017.
+ * Created by Lana Pham on 1/19/2017.
+ * CIS 320 8:00am - 9:30am
  */
-//homework part 1 - for every click, make it say hello to console
-
-//tells console to say hello
+//Part 1 - for every click, make it say hello to console
 function sayHello(event) {
     console.log("hello")
 }
-//creates button and calls for sayHello Function
+
 var formButton1 = $('#button1');
 formButton1.on("click", sayHello);
 
 //Part two - concatenate fields into box three
 function calculate(event) {
-    console.log('#field1' + '#field2');
+    var num = document.getElementById("field1").value;
+    var num2 = document.getElementById("field2").value;
+    var results = parseInt(num) + parseInt(num2);
+    document.getElementById("field3").value = results;
 }
 
 var formButton2 = $('#button2');
 formButton2.on("click", calculate);
 
-//part 3
-function hideFunction(event) {
-    $('#paragraphToHide');
+//part 3 - Toggle the paragraph to be hidden and not hidden.
+function toggleMessage(event) {
+    $('#paragraphToHide').toggle();
 }
 
 var formButton3 = $('#button3');
-formButton3.on("click", hideFunction);
+formButton3.on("click", toggleMessage);
+
+//part 4 - regular expression phone number
+function validation(event) {
+    var input = $('#phoneField').val();
+    var phoneNum = /^([0-9]{3})[-]([0-9]{3})[-]([0-9]{4})$/;
+
+    if (phoneNum.test(input)) {
+        console.log("Ok");
+    }
+    else {
+        console.log("Bad");
+    }
+}
+
+var formButton4 = $('#button4');
+formButton4.on("click", validation);
+
+
+//part 5 - JSON
+function jsonFunction (event) {
+
+    //creates an empty object
+    var formObject = {};
+    formObject.firstName = $("#firstName").val();
+    formObject.lastName = $("#lastName").val();
+    formObject.email = $("#email").val();
+
+    var jsonString = JSON.stringify(formObject);
+
+    console.log(jsonString);
+}
+
+var formButton5 = $('#button5');
+formButton5.on("click", jsonFunction);
+
+
 
 
 
